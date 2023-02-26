092253c9f9292690df3062169ffc1c3cf84dcc2e
kunhonrh321 <habeebrh321@gmail.com>
2023-02-26 20:55:32 +0530

    send messages implemented

--- web/firebase_messaging_sw.js
+++ web/firebase_messaging_sw.js
@@ -3,16 +3,16 @@ importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-comp
 
 // todo Copy/paste firebaseConfig from Firebase Console
 const firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    databaseURL: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "...",
+    apiKey: "AIzaSyD3sjRsNfna0FxJE5FL_CaskrsedNzM4B8",
+    authDomain: "push-notification-45587.firebaseapp.com",
+    projectId: "push-notification-45587",
+    storageBucket: "push-notification-45587.appspot.com",
+    messagingSenderId: "850762385412",
+    appId: "1:850762385412:web:1dc056801115d056140d51"
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // todo Set up background message handler
+
